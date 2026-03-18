66443d23a35d0b3c445fb9c23e25588844350029
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2026-03-18 17:24:48 +0100

    Add classes to models

--- mwe/polls/models.py
+++ mwe/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    class Meta:
+        permissions = (("answer", "Answer"),)
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class QuestionWithText(Question):
+    answer = models.TextField()
+
+
+class QuestionWithDuration(Question):
+    answer = models.DurationField()
