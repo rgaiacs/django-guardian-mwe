a3a0e7586252bad630b97990c16ec121cb21804d
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2026-03-19 10:58:22 +0100

    Add abstract model

--- mwe/polls/models.py
+++ mwe/polls/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-
-class Question(models.Model):
+class AbstractQuestion(models.Model):
     class Meta:
+        abstract = True
         permissions = (("answer", "Answer"),)
 
+class Question(AbstractQuestion):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
